02d26bc0211c4c0020280c5707de63778fd86cf8
Tiago Martins <tiagoammartins@gmail.com>
2021-04-30 09:44:18 -0400

    nvim: Add python and C language servers

--- editor/.config/nvim/lua/plugin/lsp.lua
+++ editor/.config/nvim/lua/plugin/lsp.lua
@@ -48,7 +48,7 @@ end
 
 -- use a loop to conveniently both setup defined servers
 -- and map buffer local keybindings when the language server attaches
-local servers = {'pyright'}
+local servers = {'pyls', 'clangd'}
 for _, lsp in ipairs(servers) do
 	nvim_lsp[lsp].setup{on_attach = on_attach}
 end
