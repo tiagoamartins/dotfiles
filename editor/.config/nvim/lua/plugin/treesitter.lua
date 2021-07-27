79489767423bd7d06b5209f4637137692746b5f9
Tiago Martins <tiagoammartins@gmail.com>
2021-07-27 08:21:54 -0400

    nvim: Add extra treesitter plugins

--- editor/.config/nvim/lua/plugin/treesitter.lua
+++ editor/.config/nvim/lua/plugin/treesitter.lua
@@ -1,9 +1,74 @@
 require'nvim-treesitter.configs'.setup{
 	ensure_installed = {'bash', 'c', 'html', 'json', 'lua', 'python', 'rst'},
+	context_commentstring = {
+		enable = true,
+	},
 	highlight = {
+		enable = true,
+	},
+	incremental_selection = {
 		enable = true,
 	},
 	indent = {
 		enable = true,
 	},
+	textobejcts = {
+		select = {
+			enable = true,
+			keymaps = {
+				["af"] = "@function.outer",
+				["if"] = "@function.inner",
+				["ac"] = "@class.outer",
+				["ic"] = "@class.inner",
+			},
+		},
+		swap = {
+			enable = true,
+			swap_next = {
+				["<leader>a"] = "@parameter.inner",
+			},
+			swap_previous = {
+				["<leader>A"] = "@parameter.inner",
+			},
+		},
+		move = {
+			enable = true,
+			set_jumps = true, -- whether to set jumps in the jumplist
+			goto_next_start = {
+				["]m"] = "@function.outer",
+				["]]"] = "@class.outer",
+			},
+			goto_next_end = {
+				["]M"] = "@function.outer",
+				["]["] = "@class.outer",
+			},
+			goto_previous_start = {
+				["[m"] = "@function.outer",
+				["[["] = "@class.outer",
+			},
+			goto_previous_end = {
+				["[M"] = "@function.outer",
+				["[]"] = "@class.outer",
+			},
+		},
+		lsp_interop = {
+			enable = true,
+			peek_definition_code = {
+				["df"] = "@function.outer",
+				["dF"] = "@class.outer",
+			},
+		},
+	},
+	refactor = {
+		highlight_definitions = { enable = true },
+	},
+	rainbow = {
+		enable = true,
+		extended_mode = true, -- Highlight also non-parentheses delimiters, boolean or table: lang -> boolean
+		max_file_lines = 1000, -- Do not enable for files with more than 1000 lines, int
+	}
+}
+
+require'treesitter-context.config'.setup{
+	enable = true,
 }
