92b78274f70ffa3c08ceb6aebce6dc4824db6cc8
Tiago Martins <tiagoammartins@gmail.com>
2021-07-27 08:18:58 -0400

    nvim: Use treesitter syntax highlight for all

--- editor/.config/nvim/lua/plugin/treesitter.lua
+++ editor/.config/nvim/lua/plugin/treesitter.lua
@@ -2,7 +2,6 @@ require'nvim-treesitter.configs'.setup{
 	ensure_installed = {'bash', 'c', 'json', 'lua', 'python', 'rst'},
 	highlight = {
 		enable = true,
-		disable = {'c', 'rust'},
 	},
 	indent = {
 		enable = true,
