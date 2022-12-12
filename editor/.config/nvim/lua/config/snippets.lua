523cff2807300a370009e45ede5e902d9887e3bc
Tiago Martins <tiagoammartins@gmail.com>
2022-12-11 21:16:24 -0500

    nvim: Add #ifdef snippet for C

--- editor/.config/nvim/lua/config/snippets.lua
+++ editor/.config/nvim/lua/config/snippets.lua
@@ -6,9 +6,11 @@ end
 local s = luasnip.snippet
 local t = luasnip.text_node
 local i = luasnip.insert_node
+local r = luasnip.restore_node
 local d = luasnip.dynamic_node
 local sn = luasnip.snippet_node
 local fmt = require('luasnip.extras.fmt').fmt
+local rep = require('luasnip.extras').rep
 
 luasnip.config.set_config({
 	history = true,
@@ -29,6 +31,23 @@ luasnip.add_snippets('all', {
 	}), {
 		condition = file_begin,
 		show_condition = file_begin
+	})
+})
+
+luasnip.add_snippets('c', {
+	s('#i', {
+		t('#ifndef '),
+		d(1, function (args)
+			filename = vim.fn.expand('%')
+			filename = filename:gsub('%.', '_')
+
+			return sn(nil, i(1, '__' .. string.upper(filename) .. '__'))
+		end, {}),
+		t({'', '#define '}),
+		rep(1),
+		t({'', '', ''}),
+		i(0),
+		t({'', '', '#endif'})
 	})
 })
 
