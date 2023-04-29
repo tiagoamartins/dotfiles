93d7acd26726535f3fabe221e159dddac4f108fb
Tiago Martins <tiagoammartins@gmail.com>
2023-04-28 20:02:36 -0400

    nvim: Add syntax related plugins to Lazy

---
+++ editor/.config/nvim/lua/plugins/syntax.lua
@@ -0,0 +1,7 @@
+return {
+	{'ARM9/arm-syntax-vim', ft = {'arm', 'armv4', 'armv5'}},
+	{'Glench/Vim-Jinja2-Syntax', ft = {'jinja'}},
+	{'Shirk/vim-gas', ft = {'gas'}},
+	{'jvirtanen/vim-hcl', ft = {'hcl'}},
+	{'tpope/vim-git', ft = {'git', 'gitcommit', 'gitconfig', 'gitrebase', 'gitsendemail'}}
+}
