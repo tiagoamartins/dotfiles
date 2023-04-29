853081397b2e4cef8268e483a17394fb7b1e4191
Tiago Martins <tiagoammartins@gmail.com>
2023-04-28 20:04:59 -0400

    nvim: Add command related plugins to Lazy

---
+++ editor/.config/nvim/lua/plugins/commands.lua
@@ -0,0 +1,20 @@
+return {
+	'igemnace/vim-makery',
+	'tpope/vim-abolish',
+	{
+		'tpope/vim-dispatch',
+		cmd = {'Dispatch', 'Make', 'Focus', 'FocusDispatch', 'Start'},
+	},
+	{
+		'tpope/vim-fugitive',
+		cmd = {
+			'Git', 'Gedit', 'Gtabedit', 'Gread', 'Gsplit', 'Gvsplit', 'Glcd', 'Glgrep',
+			'Gllog', 'GlLog', 'Gdiffsplit', 'Gvdiffsplit', 'Gclog', 'Gcd', 'Gbrowse', 'Gdrop'
+		},
+	},
+	'tpope/vim-projectionist',
+	{
+		'vim-test/vim-test',
+		cmd = {'TestClass', 'TestFile', 'TestLast', 'TestNearest', 'TestSuite', 'TestVisit'},
+	}
+}
