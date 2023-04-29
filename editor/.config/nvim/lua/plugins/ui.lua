7054c1846a37d51b3ff9d14d78eb80dad8123680
Tiago Martins <tiagoammartins@gmail.com>
2023-04-28 20:01:08 -0400

    nvim: Add UI related plugins to Lazy

---
+++ editor/.config/nvim/lua/plugins/ui.lua
@@ -0,0 +1,14 @@
+return {
+	{
+		'godlygeek/tabular',
+		cmd = 'Tabularize',
+		ft = {'systemverilog', 'verilog', 'vhdl'},
+		lazy = true
+	},
+	'tpope/vim-apathy',
+	'tpope/vim-commentary',
+	'tpope/vim-repeat',
+	'tpope/vim-speeddating',
+	'tpope/vim-surround',
+	'tpope/vim-unimpaired',
+}
