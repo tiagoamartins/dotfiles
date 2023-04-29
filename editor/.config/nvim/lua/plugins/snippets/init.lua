f2c68d8d18f7b3c170c1cf98f307908ee3487384
Tiago Martins <tiagoammartins@gmail.com>
2023-04-28 22:18:42 -0400

    nvim: Remove key mapping conflict with snippets

--- editor/.config/nvim/lua/plugins/snippets/init.lua
+++ editor/.config/nvim/lua/plugins/snippets/init.lua
@@ -11,10 +11,10 @@ return {
 			delete_check_events = 'TextChanged'
 		},
 		keys = {
-			{'<C-l>', function() return require('luasnip').choice_active() and '<Plug>luasnip-next-choice' or '<C-l>' end, expr = true, mode = {'i'}},
-			{'<C-j>', function() return require('luasnip').jumpable(1) and '<Plug>luasnip-jump' or '<C-j>' end, expr = true, mode = {'i'}},
-			{'<C-k>', function() require('luasnip').jump(-1) end, mode = {'i', 's'}},
-			{'<C-j>', function() require('luasnip').jump(1) end, mode = {'s'}}
+			{'<leader>sl', function() return require('luasnip').choice_active() and '<Plug>luasnip-next-choice' end, expr = true, mode = {'i'}},
+			{'<leader>sj', function() return require('luasnip').jumpable(1) and '<Plug>luasnip-jump' end, expr = true, mode = {'i'}},
+			{'<leader>sk', function() require('luasnip').jump(-1) end, mode = {'i', 's'}},
+			{'<leader>sj', function() require('luasnip').jump(1) end, mode = {'s'}}
 		}
 	}
 }
