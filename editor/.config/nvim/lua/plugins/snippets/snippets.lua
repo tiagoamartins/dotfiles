3f87fc83a1e00dd0adbb1b69d7a3492aa67094e1
Tiago Martins <tiagoammartins@gmail.com>
2023-05-16 07:56:36 -0400

    nvim: Add systemverilog snippets

--- editor/.config/nvim/lua/plugins/snippets/snippets.lua
+++ editor/.config/nvim/lua/plugins/snippets/snippets.lua
@@ -66,6 +66,35 @@ function M.add_snippets()
 			})
 		)
 	})
+
+	luasnip.add_snippets('systemverilog', {
+		s('cmb', fmt([[
+				always_comb begin
+					{}
+				end
+			]], {i(0)})
+		),
+		s('ff', fmt([[
+				always_ff @({7} {1}) begin
+					if ({6}) begin
+						{4} <= {5};
+					end else begin
+						{2} <= {3};
+					end
+				end
+			]], {
+				i(1, 'clk_i'),
+				i(2, 'sig'),
+				i(3, '1\'b1'),
+				rep(2),
+				i(4, '1\'b0'),
+				i(5, '!rstn_i'),
+				i(6, 'posedge')
+			})
+		)
+	})
+
+	luasnip.filetype_extend('systemverilog', {'verilog'})
 end
 
 return M
