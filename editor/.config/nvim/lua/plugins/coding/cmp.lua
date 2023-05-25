445c90aa79bce7447100ce2f702379687d3f4d94
Tiago Martins <tiagoammartins@gmail.com>
2023-05-24 21:58:52 -0400

    nvim: Fallback to tab if cmp popup is not open

--- editor/.config/nvim/lua/plugins/coding/cmp.lua
+++ editor/.config/nvim/lua/plugins/coding/cmp.lua
@@ -1,11 +1,5 @@
 local M = {}
 
-function has_words_before()
-	unpack = unpack or table.unpack
-	local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-	return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-end
-
 function M.opts()
 	local cmp = require('cmp')
 	local luasnip = require('luasnip')
@@ -35,8 +29,6 @@ function M.opts()
 					cmp.select_next_item()
 				elseif luasnip.expand_or_locally_jumpable() then
 					luasnip.expand_or_jump()
-				elseif has_words_before() then
-					cmp.complete()
 				else
 					fallback()
 				end
