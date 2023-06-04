3b3dc52b0ae4ce7fc97f589a3a13e9530f3d70b8
Tiago Martins <tiagoammartins@gmail.com>
2023-06-03 22:06:02 -0400

    nvim: Improve telescope key mappings

--- editor/.config/nvim/lua/plugins/editor.lua
+++ editor/.config/nvim/lua/plugins/editor.lua
@@ -6,22 +6,37 @@ return {
 			tscope = require('telescope')
 			tscope.setup()
 			tscope.load_extension('find_pickers')
+			tscope.load_extension('fzf')
 			tscope.load_extension('luasnip')
 			tscope.load_extension('undo')
 		end,
 		version = '0.1.x',
 		keys = {
-			{'<leader>ff', '<cmd>Telescope find_files<cr>', desc = 'Find Files'},
-			{'<leader>fg', '<cmd>Telescope live_grep<cr>'},
-			{'<leader>fb', '<cmd>Telescope buffers<cr>'},
-			{'<leader>fh', '<cmd>Telescope help_tags<cr>'},
-			{'<leader>fp', '<cmd>Telescope find_pickers<cr>'},
-			{'<leader>fs', '<cmd>Telescope luasnip<cr>'},
-			{'<leader>fu', '<cmd>Telescope undo<cr>'},
+			{'<leader>?', function()
+				require('telescope.builtin').oldfiles() end, desc =  '[?] Find recently opened files' },
+			{'<leader><space>', function() require('telescope.builtin').buffers() end, desc =  '[ ] Find existing buffers'},
+			{'<leader>/', function()
+				require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown({
+					winblend = 10,
+					previewer = false,
+				}))
+			end, desc =  '[/] Fuzzily search in current buffer'},
+			{'<leader>gf', function() require('telescope.builtin').git_files() end, desc = 'Search [G]it [F]iles'},
+			{'<leader>sf', function() require('telescope.builtin').find_files() end, desc = '[S]earch [F]iles'},
+			{'<leader>sh', function() require('telescope.builtin').help_tags() end, desc = '[S]earch [H]elp'},
+			{'<leader>sw', function() require('telescope.builtin').grep_string() end, desc = '[S]earch current [W]ord'},
+			{'<leader>sg', function() require('telescope.builtin').live_grep() end, desc = '[S]earch by [G]rep'},
+			{'<leader>sd', function() require('telescope.builtin').diagnostics() end, desc = '[S]earch [D]iagnostics'},
+			{'<leader>sp', function() require('telescope').extensions.find_pickers.find_pickers() end, desc = '[S]earch [P]ickers'},
+			{'<leader>ss', function() require('telescope').extensions.luasnip.luasnip() end, desc = '[S]earch [S]nippets'},
+			{'<leader>su', function() require('telescope').extensions.undo.undo() end, desc = '[S]earch [U]ndo tree'},
 		},
 		dependencies = {
 			'plenary.nvim',
-			'nvim-telescope/telescope-fzf-native.nvim',
+			{
+				'nvim-telescope/telescope-fzf-native.nvim',
+				build = 'make'
+			},
 			'keyvchan/telescope-find-pickers.nvim',
 			'debugloop/telescope-undo.nvim',
 			'benfowler/telescope-luasnip.nvim',
