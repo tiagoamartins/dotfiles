d8fb9dfe010b84630ca10d5c32f464b8703088ab
Tiago Martins <tiagoammartins@gmail.com>
2023-06-17 14:25:55 -0400

    nvim: Move dap setup to separate file

--- editor/.config/nvim/lua/plugins/debug/dap.lua
+++ editor/.config/nvim/lua/plugins/debug/dap.lua
@@ -1,6 +1,4 @@
-local M = {}
-
-function M.setup()
+local function config()
 	local dap = require('dap')
 
 	dap.defaults.fallback.terminal_win_cmd = [[ belowright new ]]
@@ -51,4 +49,42 @@ function M.setup()
 	vim.keymap.set('n', '<leader>R', ":RunScriptWithArgs")
 end
 
-return M
+return {
+	'mfussenegger/nvim-dap',
+	lazy = true,
+	config = config,
+	keys = {
+		{'<leader>td', function() require('dap').terminate() end, desc = '[T]erminate [D]ebugging Session'},
+		{'<leader>b', function() require('dap').toggle_breakpoint() end, desc = 'Add [B]reakpoint'},
+		{'<leader>B', function()
+			require('dap').set_breakpoint(vim.fn.input('Breakpoint condition: '))
+			end, desc = 'Add conditional [B]reakpoint'},
+		{'<leader>lp', function()
+			require('dap').set_breakpoint(nil, nil, vim.fn.input('Log point message: '))
+			end, desc = 'Add [L]og [P]oint'},
+		{'<leader>rd', function() require('dap').repl.toggle() end, desc = '[R]un [D]ebugging Session'},
+		{'<leader>ld', function() require('dap').run_last() end, desc = 'Run [L]ast [D]ebugging Session'},
+		{'<F5>', function() require('dap').continue() end, desc = 'DAP: Start/continue'},
+		{'<F6>', function() require('dap').step_over() end, desc = 'DAP: Step over'},
+		{'<F7>', function() require('dap').step_into() end, desc = 'DAP: Step into'},
+		{'<F8>', function() require('dap').step_out() end, desc = 'DAP: Step out'},
+	},
+	dependencies = {
+		{
+			'rcarriga/nvim-dap-ui',
+			keys = {
+			{'<f3>', function() require('dapui').toggle() end, desc = 'DAP: Activate UI'},
+			},
+			config = function() require('plugins.debug.dap_ui').setup() end,
+		},
+		{
+			'theHamsta/nvim-dap-virtual-text',
+			opts = require('plugins.debug.dap_vtxt').opts
+		},
+		{
+			'nvim-telescope/telescope-dap.nvim',
+			config = function() require('telescope').load_extension('dap') end,
+			cond = function() return require('config.util').has('nvim-telescope') end
+		},
+	}
+}
