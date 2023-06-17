36875f581dc548b3c5d885f7cce8abefbb9395f1
Tiago Martins <tiagoammartins@gmail.com>
2023-06-17 14:07:51 -0400

    nvim: Move grammar-guard setup to separate file

---
+++ editor/.config/nvim/lua/plugins/lsp/grammar-guard.lua
@@ -0,0 +1,28 @@
+return {
+	'brymer-meneses/grammar-guard.nvim',
+	cond = function() return vim.fn.executable('ltex-ls') == 1 end,
+	config = function(_, opts)
+		require('grammar-guard').init()
+		local nlsp = require('lspconfig')
+		nlsp.grammar_guard.setup(opts)
+	end,
+	opts = {
+		cmd = {'ltex-ls'},
+		settings = {
+			ltex = {
+				enabled = {'latex', 'tex', 'bib', 'markdown'},
+				language = 'en',
+				diagnosticSeverity = 'information',
+				setenceCacheSize = 2000,
+				additionalRules = {
+					enablePickyRules = true,
+					motherTongue = 'en',
+				},
+				trace = {server = 'verbose'},
+				dictionary = {},
+			disabledRules = {en = {'EN_QUOTES'}},
+				hiddenFalsePositives = {},
+			}
+		}
+	}
+}
