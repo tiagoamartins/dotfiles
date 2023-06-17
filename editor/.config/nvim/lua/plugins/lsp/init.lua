71aa5024cf645d5cbb7b089d12dfeb1c47f9470d
Tiago Martins <tiagoammartins@gmail.com>
2023-06-17 14:15:17 -0400

    nvim: Move mason-null-ls setup to separate file

--- editor/.config/nvim/lua/plugins/lsp/init.lua
+++ editor/.config/nvim/lua/plugins/lsp/init.lua
@@ -1,20 +1,7 @@
 return {
 	require('plugins.lsp.mason-lspconfig'),
 	require('plugins.lsp.lspconfig'),
-
-	-- formatters
-	{
-		'jay-babu/mason-null-ls.nvim',
-		event = {'BufReadPre', 'BufNewFile'},
-		dependencies = {
-			"williamboman/mason.nvim",
-			'jose-elias-alvarez/null-ls.nvim',
-		},
-		opts = {
-			ensure_installed = {'ansible-lint', 'pylint', 'yamllint'},
-			automatic_installation = false
-		}
-	},
+	require('plugins.lsp.mason-null-ls'),
 	require('plugins.lsp.null-ls'),
 	require('plugins.lsp.grammar-guard')
 }
