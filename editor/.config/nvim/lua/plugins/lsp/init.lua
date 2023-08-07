7aabdc984037f301d017d843285e93ce652dc321
Tiago Martins <tiagoammartins@gmail.com>
2023-08-07 08:09:47 -0400

    nvim: Remove Null-LS and Mason-Null-LS

--- editor/.config/nvim/lua/plugins/lsp/init.lua
+++ editor/.config/nvim/lua/plugins/lsp/init.lua
@@ -1,7 +1,5 @@
 return {
 	require('plugins.lsp.mason-lspconfig'),
 	require('plugins.lsp.lspconfig'),
-	require('plugins.lsp.mason-null-ls'),
-	require('plugins.lsp.null-ls'),
 	require('plugins.lsp.grammar-guard')
 }
