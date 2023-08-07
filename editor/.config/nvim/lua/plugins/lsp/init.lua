520365e7213f7f58dab48a17afcd92de80d10448
Tiago Martins <tiagoammartins@gmail.com>
2023-08-07 08:11:36 -0400

    nvim: Add fidget plugin

--- editor/.config/nvim/lua/plugins/lsp/init.lua
+++ editor/.config/nvim/lua/plugins/lsp/init.lua
@@ -1,5 +1,6 @@
 return {
 	require('plugins.lsp.mason-lspconfig'),
 	require('plugins.lsp.lspconfig'),
-	require('plugins.lsp.grammar-guard')
+	require('plugins.lsp.grammar-guard'),
+	require('plugins.lsp.fidget')
 }
