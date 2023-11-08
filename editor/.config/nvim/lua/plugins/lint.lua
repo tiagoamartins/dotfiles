aaf7ebf72191f8ca23a76f5b945cb17cfef21e82
Tiago Martins <tiagoammartins@gmail.com>
2023-11-07 22:53:12 -0500

    nvim: Add pylint to nvim-lint

--- editor/.config/nvim/lua/plugins/lint.lua
+++ editor/.config/nvim/lua/plugins/lint.lua
@@ -5,6 +5,22 @@ return {
 		config = function()
 			local lint = require('lint')
 
+			lint.linters_by_ft = {
+				python = {'pylint'}
+			}
+
+			local pysite = vim.fn.trim(vim.fn.system(
+				'python3 -c "import site; print(site.getsitepackages()[0])"'
+			))
+
+			if pysite ~= nil then
+				local pylint = lint.linters.pylint
+				pylint.args = {
+					'-f', 'json', '--init-hook',
+					'import sys; sys.path.append(\'' .. pysite .. '\')'
+				}
+			end
+
 			local lint_augroup = vim.api.nvim_create_augroup('lint', {clear = true})
 			vim.api.nvim_create_autocmd({'BufEnter', 'BufWritePost', 'InsertLeave'}, {
 				group = lint_augroup,
