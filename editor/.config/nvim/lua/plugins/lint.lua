b336b7dd9f774242da1dc2b6fa734a1d5a8bfdfd
Tiago Martins <tiagoammartins@gmail.com>
2023-11-07 22:52:51 -0500

    nvim: Add nvim-lint plugin

---
+++ editor/.config/nvim/lua/plugins/lint.lua
@@ -0,0 +1,21 @@
+return {
+	{
+		'mfussenegger/nvim-lint',
+		event = {'BufReadPre', 'BufNewFile'},
+		config = function()
+			local lint = require('lint')
+
+			local lint_augroup = vim.api.nvim_create_augroup('lint', {clear = true})
+			vim.api.nvim_create_autocmd({'BufEnter', 'BufWritePost', 'InsertLeave'}, {
+				group = lint_augroup,
+				callback = function()
+					lint.try_lint()
+				end
+			})
+
+			vim.keymap.set('n', '<leader>l', function()
+				lint.try_lint()
+			end, {desc = 'Run [L]int for current file'})
+		end
+	}
+}
