78ae99a1ff2c1ede07cfc30a1b41b0bf0fe2f0e4
Tiago Martins <tiagoammartins@gmail.com>
2025-01-17 22:19:50 -0500

    nvim: Remove old ARM syntax highlighting

--- editor/.config/nvim/lua/plugins/syntax.lua
+++ editor/.config/nvim/lua/plugins/syntax.lua
@@ -1,5 +1,4 @@
 return {
-	{'ARM9/arm-syntax-vim', ft = {'arm', 'armv4', 'armv5'}},
 	{'Glench/Vim-Jinja2-Syntax', ft = {'jinja'}},
 	{'Shirk/vim-gas', ft = {'gas'}},
 	{'jvirtanen/vim-hcl', ft = {'hcl'}},
