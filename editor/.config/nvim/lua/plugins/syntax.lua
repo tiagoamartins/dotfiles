344a04b471a81937ab3330bc5ba76a0b07da1f30
Tiago Martins <tiagoammartins@gmail.com>
2025-01-17 22:20:43 -0500

    nvim: Add plugin to autodetect ansible playbooks

--- editor/.config/nvim/lua/plugins/syntax.lua
+++ editor/.config/nvim/lua/plugins/syntax.lua
@@ -2,5 +2,6 @@ return {
 	{'Glench/Vim-Jinja2-Syntax', ft = {'jinja'}},
 	{'Shirk/vim-gas', ft = {'gas'}},
 	{'jvirtanen/vim-hcl', ft = {'hcl'}},
+	{'mfussenegger/nvim-ansible', ft = {'ansible'}},
 	{'tpope/vim-git', ft = {'git', 'gitcommit', 'gitconfig', 'gitrebase', 'gitsendemail'}}
 }
