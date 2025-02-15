7f46c561d8108044de46aef6f0ea6115735a5379
Tiago Martins <tiagoammartins@gmail.com>
2025-02-15 17:08:57 -0500

    nvim: Add oil.nvim keymaps

--- editor/.config/nvim/lua/plugins/editor/oil.lua
+++ editor/.config/nvim/lua/plugins/editor/oil.lua
@@ -1,15 +1,33 @@
 return {
-    'stevearc/oil.nvim',
-    dependencies = {
-        'echasnovski/mini.icons'
-    },
-    opts = {
-        columns = {
-            'icon'
-        }
-    },
-    cmd = {'Oil'},
-    keys = {
-        {'<leader>o', function() require('oil').open(nil) end, desc = '[O]pen parent directory' },
-    }
+	'stevearc/oil.nvim',
+	opts = {
+        view_options = {
+            show_hidden = true
+        },
+        keymaps = {
+            ['g?'] = 'actions.show_help',
+            ['<CR>'] = 'actions.select',
+            ['<C-v>'] = 'actions.select_vsplit',
+            ['<C-x>'] = 'actions.select_split',
+            ['<C-t>'] = 'actions.select_tab',
+            ['<C-p>'] = 'actions.preview',
+            ['<C-c>'] = 'actions.close',
+            ['<C-l>'] = 'actions.refresh',
+            ['-'] = 'actions.parent',
+            ['_'] = 'actions.open_cwd',
+            ['`'] = 'actions.cd',
+            ['~'] = 'actions.tcd',
+            ['gs'] = 'actions.change_sort',
+            ['gx'] = 'actions.open_external',
+            ['g.'] = 'actions.toggle_hidden',
+            ['g\\'] = 'actions.toggle_trash',
+        },
+        use_default_keymaps = true,
+	},
+	cmd = {'Oil'},
+	keys = {
+		{'<leader>o', function()
+            require('oil').open(nil)
+        end, desc = '[O]pen parent directory' },
+	}
 }
