5434102ff26ec6898ea7b71580e752aedb38461c
Tiago Martins <tiagoammartins@gmail.com>
2025-02-15 17:03:44 -0500

    nvim: Update blink.cmp configuration
    - Rename plugin file.
    - Remove icons.
    - Disable pre-selection.

---
+++ editor/.config/nvim/lua/plugins/blink.lua
@@ -0,0 +1,56 @@
+return {
+    'saghen/blink.cmp',
+    version = '*',
+    build = 'cargo build --release',
+    opts = {
+        keymap = {preset = 'default'},
+        sources = {
+            default = function(ctx)
+                local success, node = pcall(vim.treesitter.get_node)
+                local comments = {'comment', 'line_comment', 'block_comment'}
+
+                if success and node and vim.tbl_contains(comments, node:type()) then
+                    return {'buffer'}
+                else
+                    return {
+                        'lsp',
+                        'path',
+                        'snippets',
+                        'buffer'
+                    }
+                end
+            end,
+        },
+        completion = {
+            list = {
+                selection = {
+                    preselect = false,
+                    auto_insert = false,
+                }
+            },
+            accept = {
+                auto_brackets = {enabled = false},
+            },
+            menu = {
+                border = "rounded",
+                winhighlight = "Normal:Normal,FloatBorder:BorderBG,CursorLine:PmenuSel,Search:None",
+
+                draw = {
+                    columns = {
+                        {'label', 'label_description', gap = 1},
+                        {'kind_icon', 'kind'}
+                    },
+                    treesitter = {'lsp'},
+                }
+            },
+            -- show documentation when selecting a completion item
+            documentation = {
+                auto_show = true,
+                auto_show_delay_ms = 500
+            },
+            -- display a preview of the selected item on the current line
+            ghost_text = {enabled = true},
+        },
+    },
+    opts_extend = {'sources.default'}
+}
