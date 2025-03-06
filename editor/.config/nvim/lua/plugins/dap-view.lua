1ef0ce025718cdcff18fde19591f2850ba888572
Tiago Martins <tiagoammartins@gmail.com>
2025-03-05 22:40:59 -0500

    nvim: Use nvim-dap-view for debugging

---
+++ editor/.config/nvim/lua/plugins/dap-view.lua
@@ -0,0 +1,15 @@
+return {
+    'igorlfs/nvim-dap-view',
+    opts = {},
+    keys = {
+        {'<f3>', '<cmd>DapViewToggle<cr>', desc = 'DAP: Toggle view'},
+        {'<leader>w', '<cmd>DapViewWatch<cr>', desc = 'DAP: View [W]atch'},
+        {'<leader>K', function()
+            require('dap.ui.widgets').hover(nil, {border = 'rounded'})
+        end, desc = 'DAP: Hover [K]'},
+        {'<leader>S', function()
+            local widgets = require('dap.ui.widgets')
+            widgets.centered_float(widgets.scopes, {border = 'rounded'})
+        end, desc = 'DAP: [S]copes'},
+    },
+}
