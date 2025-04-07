3df6e834d709544e2059d843bc7323ddda2cec47
Tiago Martins <tiagoammartins@gmail.com>
2025-04-06 23:08:42 -0400

    nvim: Update commands with table structure

--- editor/.config/nvim/lua/core/commands.lua
+++ editor/.config/nvim/lua/core/commands.lua
@@ -1,3 +1,12 @@
-vim.api.nvim_create_user_command('MakeTags', function ()
-    vim.fn.jobstart('ctags -R .')
-end, {})
+local commands = {
+    tags = {
+        name = 'MakeTags',
+        func = function()
+            vim.fn.jobstart('ctags -R .')
+        end,
+    },
+}
+
+for _, c in pairs(commands) do
+    vim.api.nvim_create_user_command(c.name, c.func, c.opts or {})
+end
