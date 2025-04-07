8b16742a9ddb06931f20bd50b9567ced2b8cb3f2
Tiago Martins <tiagoammartins@gmail.com>
2025-04-06 22:53:29 -0400

    nvim: Update autocmds with table structure

--- editor/.config/nvim/lua/core/autocmds.lua
+++ editor/.config/nvim/lua/core/autocmds.lua
@@ -1,54 +1,66 @@
 local tempdirs = '/tmp/*,$TMPDIR/*,$TMP/*,$TEMP/*,*/shm/*,*/tmp/*,/private$TMPDIR/*'
+local augroups = {}
 
-vim.api.nvim_create_augroup('line_return', {clear = true})
-vim.api.nvim_create_autocmd('BufReadPost', {
-    group = 'line_return',
-    pattern = '*',
-    callback = function ()
-        local pos_exit = vim.fn.line('\'\"')
-        local pos_last = vim.fn.line('$')
+augroups.editing = {
+    restore_cursor = {
+        event = {'BufReadPost'},
+        pattern = '*',
+        callback = function ()
+            local pos_exit = vim.fn.line('\'\"')
+            local pos_last = vim.fn.line('$')
 
-        if pos_exit > 0 and pos_exit <= pos_last then
-            vim.cmd.normal('g`\"zvzz')
+            if pos_exit > 0 and pos_exit <= pos_last then
+                vim.cmd.normal('g`\"zvzz')
+            end
         end
-    end
-})
+    },
+}
 
-vim.api.nvim_create_augroup('swapskip', {clear = true})
-vim.api.nvim_create_autocmd({'BufNewFile', 'BufReadPre'}, {
-    group = 'swapskip',
-    pattern = tempdirs,
-    callback = function ()
-        vim.opt_local.swapfile = false
-    end
-})
-
-vim.api.nvim_create_augroup('undoskip', {clear = true})
-vim.api.nvim_create_autocmd('BufWritePre', {
-    group = 'undoskip',
-    pattern = tempdirs,
-    callback = function ()
-        vim.opt_local.undofile = false
-    end
-})
-
-
-vim.api.nvim_create_augroup('viminfoskip', {clear = true})
-vim.api.nvim_create_autocmd({'BufNewFile', 'BufReadPre'}, {
-    group = 'viminfoskip',
-    pattern = tempdirs,
-    callback = function ()
-        vim.opt_local.viminfo = ''
-    end
-})
-
-vim.api.nvim_create_augroup('yank', {clear = true})
-vim.api.nvim_create_autocmd('TextYankPost', {
-    group = 'yank',
-    pattern = '*',
-    callback = function ()
-        if vim.v.event.operator == 'y' and vim.v.event.regname == '+' then
-            require('core.yank').osc52(vim.v.event.regcontents)
+augroups.secret = {
+    disable_swapfile = {
+        event = {'BufNewFile', 'BufReadPre'},
+        pattern = tempdirs,
+        callback = function ()
+            vim.opt_local.swapfile = false
         end
+    },
+    disable_undofile = {
+        event = {'BufWritePre'},
+        pattern = tempdirs,
+        callback = function ()
+            vim.opt_local.undofile = false
+        end
+    },
+    disable_viminfo = {
+        event = {'BufNewFile', 'BufReadPre'},
+        pattern = tempdirs,
+        callback = function ()
+            vim.opt_local.viminfo = ''
+        end
+    },
+}
+
+augroups.yank = {
+    osc52 = {
+        event = {'TextYankPost'},
+        pattern = '*',
+        callback = function ()
+            if vim.v.event.operator == 'y' and vim.v.event.regname == '+' then
+                require('core.yank').osc52(vim.v.event.regcontents)
+            end
+        end
+    },
+}
+
+for group, cmds in pairs(augroups) do
+    local augroup = vim.api.nvim_create_augroup('AU_' .. group, {clear = true})
+
+    for _, opts in pairs(cmds) do
+        local event = opts.event
+
+        opts.event = nil
+        opts.group = augroup
+
+        vim.api.nvim_create_autocmd(event, opts)
     end
-})
+end
