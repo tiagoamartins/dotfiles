b426ab62894b1cd4dba27cbf54ab86ca48b04d73
Tiago Martins <tiagoammartins@gmail.com>
2025-05-06 22:38:01 -0400

    nvim: Update Lua tabs to spaces

--- editor/.config/nvim/init.lua
+++ editor/.config/nvim/init.lua
@@ -10,13 +10,13 @@ vim.g.loaded_python_provider = 0
 -- load default python 3
 local python_prog = {}
 for str in string.gmatch(vim.fn.system({'which', '-a', 'python3'}), '([^\n]+)') do
-	table.insert(python_prog, str)
+    table.insert(python_prog, str)
 end
 
 if vim.env['VIRTUAL_ENV'] ~= nil then
-	vim.g.python3_host_prog = python_prog[2]
+    vim.g.python3_host_prog = python_prog[2]
 else
-	vim.g.python3_host_prog = python_prog[1]
+    vim.g.python3_host_prog = python_prog[1]
 end
 
 -- disable ruby support
