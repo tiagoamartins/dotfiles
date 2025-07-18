02079b11296e70e8163f838d848bd26482c9d203
Tiago Martins <tiagoammartins@gmail.com>
2025-07-17 22:11:03 -0400

    nvim: Update lua LSP config for neovim support

--- editor/.config/nvim/lsp/lua-language-server.lua
+++ editor/.config/nvim/lsp/lua-language-server.lua
@@ -7,4 +7,44 @@ return {
         '.luarc.json',
         '.luarc.jsonrc',
     },
+    settings = {
+        Lua = {
+            diagnostics = {
+                globals = {
+                    'vim',
+                },
+            },
+            runtime = {
+                -- tell the language server which version of Lua you're
+                -- using (most likely LuaJIT in the case of Neovim)
+                version = 'LuaJIT',
+                -- tell the language server how to find Lua modules same way
+                -- as Neovim (see `:h lua-module-load`)
+                path = {
+                    'lua/?.lua',
+                    'lua/?/init.lua',
+                },
+            },
+            -- make the server aware of Neovim runtime files
+            workspace = {
+                checkThirdParty = false,
+                library = {
+                    vim.env.VIMRUNTIME,
+                    "${3rd}/luv/library",
+                },
+            },
+        },
+    },
+    on_init = function(client)
+        if client.workspace_folders then
+            local path = client.workspace_folders[1].name
+            if
+                path ~= vim.fn.stdpath('config')
+                        and (vim.uv.fs_stat(path .. '/.luarc.json')
+                             or vim.uv.fs_stat(path .. '/.luarc.jsonc'))
+            then
+                return
+            end
+        end
+    end,
 }
