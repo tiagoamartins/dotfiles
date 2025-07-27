85a10edce2c530f15f5d9f9f9ae9f81f2ea2f382
Tiago Martins <tiagoammartins@gmail.com>
2025-07-27 14:00:54 -0400

    nvim: Use nvim-cmp as completion plugin

---
+++ editor/.config/nvim/lua/plugins/cmp.lua
@@ -0,0 +1,19 @@
+return {
+    {
+        'hrsh7th/nvim-cmp',
+        enabled = true,
+        lazy = false,
+        priority = 100,
+        dependencies = {
+            'onsails/lspkind.nvim',
+            'hrsh7th/cmp-nvim-lsp',
+            'hrsh7th/cmp-path',
+            'hrsh7th/cmp-buffer',
+            'L3MON4D3/LuaSnip',
+            'saadparwaiz1/cmp_luasnip',
+        },
+        config = function()
+            require('config.completion')
+        end,
+    },
+}
