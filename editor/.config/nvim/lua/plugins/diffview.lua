1473dce2a252fe5cc5adc3f23948822400adf5f3
Tiago Martins <tiagoammartins@gmail.com>
2026-03-02 20:02:53 -0500

    nvim: Update diffview mapping/commands

--- editor/.config/nvim/lua/plugins/diffview.lua
+++ editor/.config/nvim/lua/plugins/diffview.lua
@@ -1,9 +1,20 @@
 return {
     'sindrets/diffview.nvim',
+    cmd = {
+        'DiffviewClose',
+        'DiffviewFileHistory',
+        'DiffviewFocusFiles',
+        'DiffviewLog',
+        'DiffviewOpen',
+        'DiffviewRefresh',
+        'DiffviewToggleFiles',
+    },
     keys = {
         {'<leader>db', '<cmd>DiffviewFileHistory<cr>', desc = '[D]iff view [B]ranch'},
-        {'<leader>df', '<cmd>DiffviewFileHistory %<cr>', desc = '[D]iff view [F]ile'},
         {'<leader>dcl', '<cmd>DiffviewClose<cr>', desc = '[D]iff view [CL]ose'},
+        {'<leader>df', '<cmd>DiffviewFileHistory %<cr>', desc = '[D]iff view [F]ile'},
+        {'<leader>do', '<cmd>DiffviewOpen<cr>', desc = '[D]iff view [O]pen'},
+        {'<leader>dr', '<cmd>DiffviewRefresh<cr>', desc = '[D]iff view [R]efresh'},
     },
     opts = {
         use_icons = false
