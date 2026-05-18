bf837314bd6763c7217a1c72b19d6dd9f0f8379d
Tiago Martins <tiagoammartins@gmail.com>
2026-05-18 09:53:55 -0400

    nvim: Add pack commands

--- editor/.config/nvim/lua/core/commands.lua
+++ editor/.config/nvim/lua/core/commands.lua
@@ -1,4 +1,16 @@
 local commands = {
+    pack_show = {
+        name = 'PackShow',
+        func = function ()
+            vim.pack.update(nil, {offline = true})
+        end
+    },
+    pack_update = {
+        name = 'PackUpdate',
+        func = function ()
+            vim.pack.update(nil)
+        end,
+    },
     tags = {
         name = 'MakeTags',
         func = function()
