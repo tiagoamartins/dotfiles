b81d60609bb2526054da1eb5ba7b17b4aa477f73
Tiago Martins <tiagoammartins@gmail.com>
2026-07-29 08:06:22 -0400

    nvim: Update opencode keymapping

--- editor/.config/nvim/plugin/opencode.lua
+++ editor/.config/nvim/plugin/opencode.lua
@@ -5,9 +5,10 @@ vim.o.autoread = true -- Required for `opts.events.reload`
 local mappings = {
     {'<leader>Oa', function() require('opencode').ask('@this: ', {submit = true}) end, desc = 'Ask opencode…'},
     {'<leader>Ox', function() require('opencode').select() end, desc = 'Execute opencode action…'},
-    {'<leader>Ot', function() require('opencode').toggle() end, desc = 'Toggle opencode'},
     {'<leader>go',  function() return require('opencode').operator('@this ') end, desc = 'Add range to opencode', expr = true},
     {'<leader>goo', function() return require('opencode').operator('@this ') .. '_' end, desc = 'Add line to opencode', expr = true},
+    {'<leader>Ou', function() require('opencode').command('session.half.page.up') end, desc = 'Scroll OpenCode up'},
+    {'<leader>Od', function() require('opencode').command('session.half.page.down') end, desc = 'Scroll OpenCode down'},
 }
 
 for _, opts in pairs(mappings) do
